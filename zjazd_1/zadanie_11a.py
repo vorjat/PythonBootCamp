93c03bebfbb62324190e6f776affb86cd2a64105
vorjat <przemyslaw.jarek@gmail.com>
2018-10-06 16:20:03 +0200

    poprawki w 11a

--- zjazd_1/zadanie_11a.py
+++ zjazd_1/zadanie_11a.py
@@ -15,11 +15,11 @@ elif X >= int(0.9*MapX) and Y >= int(0.1*MapY) and Y <= int(0.9*MapY):
     print("Prawa krawędź")
 elif X>=int(0.9*MapX) and Y<=int(0.1*MapY):
     print("Prawy dolny róg")
-elif Y<=int(0.1*MapY) and X>=10 and X<=int(0.9*MapX):
+elif Y<=int(0.1*MapY) and X>=int(0.1*MapX) and X<=int(0.9*MapX):
     print ("Dolna krawędź")
 elif Y<=int(0.1*MapY) and X <=int(0.1*MapX):
     print ("Lewy dolny róg")
-elif X<=int(0.1*MapX) and Y>=int(0.1*MapY) and Y<=90:
+elif X<=int(0.1*MapX) and Y>=int(0.1*MapY) and Y<=int(0.9*MapY):
     print ("Lewa krawędź")
 elif X<=int(0.1*MapX) and Y>=int(0.9*MapY):
     print("Lewy górny róg")
