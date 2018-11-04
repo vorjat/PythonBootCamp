ac1df2a3d5d60b9c8e09c7fdbfb7450a73c54aeb
vorjat <przemyslaw.jarek@gmail.com>
2018-11-04 16:49:49 +0100

    zjazd 3 dzien 2 com2

--- zjazd_3/pliki/zadanie_3.py
+++ zjazd_3/pliki/zadanie_3.py
@@ -1,11 +1,21 @@
+import sys
+
+if len(sys.argv) == 3:
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+else:
+    print("Nieprawidlowa ilosc parametrow!")
+
 mails = []
 
-with open("dane/emails.txt") as f:
+with open(input_file) as f:
     for line in f:
         cleaned_line = line.strip().lower()
         if cleaned_line not in mails and line.count('@') == 1:
             mails.append(cleaned_line)
 
 mails.sort()
-for m in mails:
-    print(m)
+
+with open(output_file, 'w') as f:
+    for m in mails:
+        f.write(m+"\n")
